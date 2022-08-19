4eca60b5e2df9a06c700815e00609f12ecfafe9b
deon <mkandaniel@gmail.com>
2022-08-19 00:28:39 -0700

    attempt at pdf converter

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ selected keywords with asterisks.
 import traceback
 from flask import Flask, render_template, request, redirect
 import requests
+import PyPDF2
 
 app = Flask(__name__)
 KANYE_URI = "https://api.kanye.rest/"
@@ -33,6 +34,27 @@ def edit():
             with open(file_path, 'w', encoding="utf-8") as new_doc:
                 new_doc.write(file_to_parse)
             return redirect('/confirmation')
+        except FileNotFoundError:
+            return traceback.print_exc()
+
+
+@app.route('/pdfedit', methods=["POST"])
+def pdfedit():
+    """Function attempts to access specified file from path and replace
+    given keyword with asterisks"""
+    if request.method == 'POST':
+        try:
+            file_path = request.form['path']
+            keyword = request.form['keyword']
+            with open("/static/{file_path}", "w") as new_doc:
+                pdf_file_obj = open(file_path, "rb")
+                pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+                for x in range(pdf_reader.numPages):
+                    page_obj = pdf_reader.getPage(x)
+                    page_text = page_obj.extract_text()
+                    page_text = page_text.replace(keyword, "*****")
+                    new_doc.write(page_text)
+                return redirect('/confirmation')
         except FileNotFoundError:
             return traceback.print_exc()
 
