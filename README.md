d39f7865b1b8a1bca982192f35b14c1cad36dac6
deon <mkandaniel@gmail.com>
2022-08-19 00:06:03 -0700

    Complete MVP

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python_project
+
+You’ve been hired as an entry level coder for the IMF, an elite, top-secret espionage and covert operations agency that handles dangerous and highly sensitive international missions that have been deemed "impossible". Your mission, should you choose to accept it, is to write a program that can scan top-secret documents and replace any/all instances of words (or phrases) that the user inputs with asterisks. Be sure only the new file remains when you’re done.
+
--- app.py
+++ app.py
@@ -1,33 +1,55 @@
-"""• You’ve been hired as an entry level coder for the IMF, an elite, top-secret 
-    espionage and covert operations agency that handles dangerous and highly 
-    sensitive international missions that have been deemed "impossible". Your mission, 
-    should you choose to accept it, is to write a program that can scan top-secret 
-    documents and replace any/all instances of words (or phrases) that the user 
-    inputs with asterisks. Be sure only the new file remains when you’re done."""
+#!/usr/bin/env python3
+"""
+This Python Script accepts a .txt file from a user and censors
+selected keywords with asterisks.
+"""
 
+import traceback
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
+KANYE_URI = "https://api.kanye.rest/"
+
 
 @app.route('/')
 def index():
+    """Default landing page that renders index template"""
     return render_template('index.html')
+
 
 @app.route('/edit', methods=["POST"])
 def edit():
+    """Function attempts to access specified file from path and replace
+    given keyword with asterisks"""
     if request.method == 'POST':
         try:
             file_path = request.form['path']
             keyword = request.form['keyword']
-            with open(file_path, 'r') as doc:
+            with open(file_path, 'r', encoding="utf-8") as doc:
                 file_to_parse = doc.read()
 
             file_to_parse = file_to_parse.replace(keyword, "*****")
-            with open(file_path, 'w') as new_doc:
+            with open(file_path, 'w', encoding="utf-8") as new_doc:
                 new_doc.write(file_to_parse)
-            
-            return redirect('/')
-        except:
-            return("something went wrong")
+            return redirect('/confirmation')
+        except FileNotFoundError:
+            return traceback.print_exc()
+
+
+@app.route('/confirmation')
+def confirm():
+    """This function renders a completion page for the edit"""
+    return render_template('confirmation.html')
+
+
+@app.route('/kanye')
+def kanye_quote():
+    """Function is a api call to Kanye quotes API"""
+    resp = requests.get(KANYE_URI)
+    quote = resp.json()["quote"]
+    return render_template('index.html', quote=quote)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
